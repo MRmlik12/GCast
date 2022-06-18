bdf197c5b03f100460044219a0278d287cb897d5
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2022-06-18 18:47:02 +0200

    Add device info from google chrome device (#2)

---
+++ src/GCast.Protocol/Discovery/Models/DeviceModel.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace GCast.Protocol.Discovery.Models;
+
+public class DeviceModel
+{
+    [JsonProperty("bssid")] public string? BssId { get; set; }
+
+    [JsonProperty("name")] public string? Name { get; set; }
+    
+    [JsonProperty("ip_address")] public string? IpAddress { get; set; }
+
+    [JsonProperty("mac_address")] public string? MacAddress { get; set; }
+
+    [JsonProperty("public_key")] public string? PublicKey { get; set; }
+
+    [JsonProperty("build_version")] public string? BuildVersion { get; set; }
+
+    [JsonProperty("version")] public int Version { get; set; }
+
+    [JsonProperty("uptime")] public decimal Uptime { get; set; }
+
+    public DiscoveryDeviceModel? DiscoveryModel { get; set; }
+}
