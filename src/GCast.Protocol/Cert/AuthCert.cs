6c180cedfbf9677dd59cb855624a4458963f9548
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2022-06-30 09:34:01 +0200

    Get peer certificate from device (#4)

---
+++ src/GCast.Protocol/Cert/AuthCert.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace GCast.Protocol.Cert;
+
+public static class AuthCert
+{
+    private const string DllName = "Libs/gcast_protocol_certificates.dll";
+
+    [DllImport(DllName)]
+    private static extern IntPtr GetDevicePeerCertificate(string ip);
+
+    public static string? GetCert(string ip)
+    {
+        var cert = GetDevicePeerCertificate(ip);
+        
+        return Marshal.PtrToStringAnsi(cert);
+    }
+}
---
+++ test/GCast.Protocol.Tests/Cert/AuthCertTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+using GCast.Protocol.Cert;
+using Xunit;
+
+namespace GCast.Protocol.Test.Cert;
+
+public class AuthCertTests
+{
+    private const string CertPattern =
+        @"^-----BEGIN CERTIFICATE-----([\s\S]*)-----END CERTIFICATE-----\s?$";
+    
+    [Fact, Trait("type", "native-utils")]
+    public void GetPeerCert_CheckIfContainsHeaders()
+    {
+        var cert = AuthCert.GetCert("192.168.1.22");
+        var isMatches = cert != null && Regex.IsMatch(cert, CertPattern);
+        
+        Assert.True(isMatches);
+    }
+}
